fca12d80d18777cbbd45743285cddd9f5eb069d7
lucaslira <lucasliraone@gmail.com>
2023-11-22 21:08:39 -0300

    Criação do banco de dados e migration users

--- src/database/knex/migrations/20231122185755_createUsers.js
+++ src/database/knex/migrations/20231122185755_createUsers.js
@@ -1,7 +1,15 @@
-exports.up = knex => knex.schema.createTable('notes', table => {
-    
+exports.up = knex => knex.schema.createTable('users', table => {
+    table.increments('id');
+    table.text('name').notNullable();
+    table.text('email').notNullable();
+    table.text('password').notNullable();
+
+    table.enum('role', ['admin', 'customer'], { useNative: true, enumName: 'roles' }).notNullable().default('customer');
+
+    table.timestamp("created_at").default(knex.fn.now());
+    table.timestamp("updated_at").default(knex.fn.now());
 });
 
-exports.down = knex => knex.schema.dropTable('notes', table => {
+exports.down = knex => knex.schema.dropTable('users', table => {
     
 });
