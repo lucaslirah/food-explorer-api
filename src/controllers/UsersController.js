d0c08e3f3e83e831475753fe89f3c9de1c6c4ed0
lucaslira <lucasliraone@gmail.com>
2023-11-22 22:03:21 -0300

    Função Create de UsersController

--- src/controllers/UsersController.js
+++ src/controllers/UsersController.js
@@ -1,14 +1,31 @@
-const AppError = require('../utils/AppError')
+const AppError = require('../utils/AppError');
+const knex = require('../database/knex');
+const { hash } = require('bcryptjs');
 
 class UsersController{
-    create(request, response){
+    async create(request, response){
         const { name, password, email } = request.body;
 
+        
         if(!name){
-            throw new AppError("Por favor, insira um nome.");
+            throw new AppError("Por favor, insira um nome!");
+        }
+        
+        const [emailExists] = await knex('users').where({ email });
+
+        if(emailExists){
+            throw new AppError('Email já em uso!');
         }
 
-        response.status(201).json({ name, password, email });
+        const hashedPassword = await hash(password, 8);
+        
+        await knex('users').insert({
+            name,
+            email,
+            password: hashedPassword
+        });
+
+        response.status(201).json();
     }
 }
 
