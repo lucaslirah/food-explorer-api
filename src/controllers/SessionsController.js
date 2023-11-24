fb6c7352de2e04124fafde23c1629f9f6b099864
lucaslira <lucasliraone@gmail.com>
2023-11-24 12:34:03 -0300

    Middleware de autenticação e SessionsController - Rotas da sessão

--- src/controllers/SessionsController.js
+++ src/controllers/SessionsController.js
@@ -1,5 +1,7 @@
-const knex = require('../database/knex');
 const AppError = require('../utils/AppError');
+const authConfig = require('../configs/auth');
+const knex = require('../database/knex');
+const { sign } = require('jsonwebtoken');
 const { compare } = require('bcryptjs');
 
 class SessionsController{
@@ -17,8 +19,23 @@ class SessionsController{
         if(!matchPassword){
             throw new AppError('Email e/ou senha incorretos.', 401);
         }
+
+        const { secret, expiresIn } = authConfig.jwt;
+        const token = sign({ role: user.role }, secret, {
+            subject: String(user.id),
+            expiresIn
+        });
+
+        response.cookie('secret', token, {
+            httpOnly: true,
+            sameSite: 'none',
+            secure: true,
+            maxAge: 15 * 60 * 1000
+        });
+
+        delete user.password;
         
-        return response.status(201).json(user);
+        return response.status(201).json({ user });
     }
 }
 
