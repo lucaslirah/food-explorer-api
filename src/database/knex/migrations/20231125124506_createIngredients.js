6b87a6e734fee0f245919b8cefc7abd1ce8d447d
lucaslira <lucasliraone@gmail.com>
2023-11-25 09:47:09 -0300

    Alteração na tabela de Ingredientes

--- src/database/knex/migrations/20231125124506_createIngredients.js
+++ src/database/knex/migrations/20231125124506_createIngredients.js
@@ -3,7 +3,6 @@ exports.up = knex => knex.schema.createTable('ingredients', table => {
     table.text('name').notNullable();
 
     table.integer('dish_id').references('id').inTable('dishes').onDelete('CASCADE');
-    table.integer('user_id').references('id').inTable('users');
 });
 
 exports.down = knex => knex.schema.dropTable('ingredients', table => {
