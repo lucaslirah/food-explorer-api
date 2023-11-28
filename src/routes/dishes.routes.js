47537948e37d4bba93c98d5a18b1b2f6a5ceafc5
lucaslira <lucasliraone@gmail.com>
2023-11-27 22:02:13 -0300

    Upload de imagens - carregando e salvando no banco

--- src/routes/dishes.routes.js
+++ src/routes/dishes.routes.js
@@ -1,11 +1,16 @@
 const { Router } = require('express');
+const multer = require('multer');
+const uploadConfig = require('../configs/upload');
 
 const DishesController = require('../controllers/DishesController');
+const DishesPicturesController = require('../controllers/DishesPicturesController');
 const ensureAuthenticated = require('../middlewares/ensureAuthenticated');
 
 const dishesRoutes = Router();
+const upload = multer(uploadConfig.MULTER);
 
 const dishesController = new DishesController();
+const dishesPicturesController = new DishesPicturesController();
 
 dishesRoutes.use(ensureAuthenticated);
 
@@ -14,6 +19,7 @@ dishesRoutes.get('/:id', dishesController.show);
 dishesRoutes.delete('/:id', dishesController.delete);
 dishesRoutes.get('/', dishesController.index);
 dishesRoutes.put('/:id', dishesController.update);
+dishesRoutes.patch('/picture', upload.single('picture'), dishesPicturesController.update);
 
 
 module.exports = dishesRoutes;
