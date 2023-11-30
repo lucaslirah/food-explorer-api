abef57121c903426048b4fe60fef93db4134364e
lucaslira <lucasliraone@gmail.com>
2023-11-30 10:54:13 -0300

    Consertando erro no knex de useNullAsDefault

--- knexfile.js
+++ knexfile.js
@@ -9,10 +9,11 @@ module.exports = {
     },
     pool: {
       afterCreate: (conn, cb) => conn.run('PRAGMA foreign_keys = ON', cb)
-      }
+      
     },
     migrations: {
       directory: path.resolve(__dirname, 'src', 'database', 'knex', 'migrations')
     },
     useNullAsDefault: true
   }
+};
