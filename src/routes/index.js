5260fa66aa46d7a5e7df738d9c81336f8511f73a
lucaslira <lucasliraone@gmail.com>
2023-11-29 21:46:28 -0300

    Métodos para deletar e criar ingredientes

--- src/routes/index.js
+++ src/routes/index.js
@@ -3,11 +3,13 @@ const { Router } = require('express');
 const usersRouter = require('./users.routes');
 const sessionsRouter = require('./sessions.routes');
 const dishesRouter = require('./dishes.routes');
+const ingredientsRouter = require('./ingredients.routes');
 
 const routes = Router();
 
 routes.use('/users', usersRouter);
 routes.use('/sessions', sessionsRouter);
 routes.use('/dishes', dishesRouter);
+routes.use('/ingredients', ingredientsRouter);
 
 module.exports = routes;
