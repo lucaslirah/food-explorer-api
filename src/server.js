1893e4114a753ae910f13716e4067e0aa8bc2ae2
lucaslira <lucasliraone@gmail.com>
2024-01-11 10:41:13 -0300

    Cors instalado

--- src/server.js
+++ src/server.js
@@ -7,8 +7,11 @@ const routes = require('./routes');
 const AppError = require('./utils/AppError');
 const uploadConfig = require('./configs/upload');
 
+const cors = require("cors");
+
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 app.use(routes);
 app.use('/files', express.static(uploadConfig.UPLOADS_FOLDER));
