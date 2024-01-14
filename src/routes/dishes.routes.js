1698c03def7f9d05114fba8878a77a293b4ffb6c
lucaslira <lucasliraone@gmail.com>
2024-01-14 11:29:52 -0300

    Aplicando autorização para rotas específicas

--- src/routes/dishes.routes.js
+++ src/routes/dishes.routes.js
@@ -7,6 +7,7 @@ const uploadConfig = require('../configs/upload');
 const DishesController = require('../controllers/DishesController');
 
 const ensureAuthenticated = require('../middlewares/ensureAuthenticated');
+const verifyUserAuthorization = require('../middlewares/verifyUserAuthorization');
 
 const dishesRoutes = Router();
 const upload = multer(uploadConfig.MULTER);
@@ -14,10 +15,10 @@ const dishesController = new DishesController();
 
 dishesRoutes.use(ensureAuthenticated);
 
-dishesRoutes.post('/create', upload.single('picture'),dishesController.create);
+dishesRoutes.post('/create', verifyUserAuthorization("admin"), upload.single('picture'), dishesController.create);
 dishesRoutes.get('/:id', dishesController.show);
-dishesRoutes.delete('/:id', dishesController.delete);
+dishesRoutes.delete('/:id', verifyUserAuthorization("admin"), dishesController.delete);
 dishesRoutes.get('/', dishesController.index);
-dishesRoutes.put('/edit/:id', upload.single('picture'), dishesController.update);
+dishesRoutes.put('/edit/:id', verifyUserAuthorization("admin"), upload.single('picture'), dishesController.update);
 
 module.exports = dishesRoutes;
